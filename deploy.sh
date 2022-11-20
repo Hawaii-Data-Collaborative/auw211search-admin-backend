0d156d52d2f692036dc0bdaa05d1c8dab1930d5d
Kyle Mulleady <kyle@windwardapps.com>
2022-11-20 09:25:37 -1000

    logging

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+echo "[deploy] ssh'ing ..."
+ssh wwa bash << EOF
+cd /var/www/auwsearch.windwardapps.com/auw-admin
+echo "[deploy] pulling code ..."
+git pull
+echo "[deploy] installing dependencies ..."
+/home/kyle/bin/yarn
+echo "[deploy] compiling ..."
+npx prisma generate
+echo "[deploy] restarting ..."
+sudo service auw-admin restart
+EOF
+
+echo "[deploy] done"
--- src/prisma.js
+++ src/prisma.js
@@ -1,3 +1,3 @@
 const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient({ log: ['query'] })
+const prisma = new PrismaClient()
 exports.prisma = prisma
