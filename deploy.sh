5f230f6a94e2bfdac016990d6c972fbafde0f17c
Kyle Mulleady <kmulleady@hawaiidata.org>
2023-11-17 15:14:06 -1000

    config

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 echo "[deploy] pushing code ..."
 git push
 echo "[deploy] ssh'ing ..."
-ssh wwa bash << EOF
-cd /var/www/auwsearch.windwardapps.com/auw-admin
+ssh auw1 bash << EOF
+cd /var/www/searchengine-admin-backend
 echo "[deploy] pulling code ..."
 git pull
 echo "[deploy] installing dependencies ..."
-/home/kyle/bin/yarn --production
+npm install --production
 echo "[deploy] compiling ..."
 npx prisma generate
 echo "[deploy] restarting ..."
-sudo service auw-admin restart
+sudo ./restart.sh
 EOF
 
 echo "[deploy] done"
