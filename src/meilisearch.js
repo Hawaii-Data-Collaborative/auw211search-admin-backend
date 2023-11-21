b92ac3621cdad2b45734ad8bb13b3c50ec9dccd2
Kyle Mulleady <kmulleady@hawaiidata.org>
2023-11-20 15:09:11 -1000

    MEILISEARCH_KEY

--- src/meilisearch.js
+++ src/meilisearch.js
@@ -1,6 +1,8 @@
 const { MeiliSearch } = require('meilisearch')
 
 const host = process.env.MEILISEARCH_HOST || 'http://localhost:7700'
-const meilisearch = new MeiliSearch({ host })
+const apiKey = process.env.MEILISEARCH_KEY
+
+const meilisearch = new MeiliSearch({ host, apiKey })
 
 module.exports = meilisearch
